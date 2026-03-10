3040b5e25f1171b69c15c40c88cc1756ade3faa0
Pino <pino0624forwork@gmail.com>
2026-03-10 08:05:15 +0800

    feat(qa): 新增 QA 區塊與 FAQ 手風琴元件

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import SocialProof from './components/SocialProof';
 import Features from './components/Features';
 import UseCases from './components/UseCases';
 import Pricing from './components/Pricing';
+import QA from './components/QA';
 import CallToAction from './components/CallToAction';
 import Footer from './components/Footer';
 
@@ -17,6 +18,7 @@ function App() {
                 <Features />
                 <UseCases />
                 <Pricing />
+                <QA />
                 <CallToAction />
             </main>
             <Footer />
